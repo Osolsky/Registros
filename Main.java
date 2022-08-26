0989889f295534b3a47fc2d8907d468dcf944d46
Yael Alfonseca <yaelsaidcecyt03@gmail.com>
2022-08-26 00:19:36 -0500

    2cond modularyzating

--- Main.java
+++ Main.java
@@ -6,56 +6,31 @@ public class Main {
     public static void main(String[] args) {
         try {
             //Instancing the objects we will need
-            FileWriter create_File = new FileWriter("E:\\Content\\correos.txt", true);
-            PrintWriter write_File = new PrintWriter(create_File);
-            Scanner scanner = new Scanner(System.in);
             File workingFile = new File("E:\\Content\\correos.txt");
+            Scanner scanner = new Scanner(System.in);
+            MasterOfRegister registerControl = new MasterOfRegister();
 
-
-
-            //Variable to see the times the user have added new data
-            int count_Data = 0;
             //Loop to add whatever data the user wants
             boolean end = false;
             while (!end) {
 
-                System.out.println("escribe 'add' para introducir un registro");
-                System.out.println("escribe 'view' para ver tus registros");
-                System.out.println("escribe 'end' para finalizar el proceso");
+                registerControl.userComunication();
                 String line = scanner.nextLine();
-                int numOfRegisters = 0;
+
 
 
                 switch (line){
 
                     case "add":
-                        count_Data++;
-                        System.out.println("Introduce tu registro");
-                        String register = scanner.nextLine();
-                        write_File.println(register);
-                        System.out.println("register saved successfully");
+                        registerControl.addRegister();
                         break;
 
                     case "view":
-                        BufferedReader readerOfWorkingFile = new BufferedReader(new FileReader(workingFile));
-                        String lineOfWorkingFile;
-                        Vector<String> vectorWhitFileContent = new Vector<String>(40);
-                        while ((lineOfWorkingFile = readerOfWorkingFile.readLine()) != null) {
-                            vectorWhitFileContent.addElement(lineOfWorkingFile);
-                        }
-
-                        String[] arrayOfVectorWhitFileContent = vectorWhitFileContent.toArray(new String[vectorWhitFileContent.size()]);
-                        for (int i = 0; i < arrayOfVectorWhitFileContent.length; i++) {
-                            ++numOfRegisters;
-                            System.out.println(numOfRegisters + ".- " + arrayOfVectorWhitFileContent[i]);
-                            System.out.println("------------------------------------------------------------");
-                        }
+                        registerControl.seeRegisters();
                         break;
 
                     case "end":
-                        end = true;
-                        create_File.close();
-                        break;
+                    end = true;
                 }
             }
 
