18404d5b07cbd7fffa17ec642d90cb51b786b6f3
Yael Alfonseca <yaelsaidcecyt03@gmail.com>
2022-08-25 22:00:19 -0500

    New class Format of Registers

---
+++ FormatOfRegisters.java
@@ -0,0 +1,52 @@
+public class FormatOfRegisters {
+    private String nombre;
+    private String apellidos;
+    private String direccion;
+    private String numeroTelefonico;
+    private String correoElectronico;
+
+    public String getNombre() {
+        return nombre;
+    }
+
+    public void setNombre(String nombre) {
+        System.out.println("Introduce tu nombre");
+        this.nombre = nombre;
+    }
+
+    public String getApellidos() {
+        return apellidos;
+    }
+
+    public void setApellidos(String apellidos) {
+        System.out.println("Introduce tus Apellidos");
+        this.apellidos = apellidos;
+    }
+
+    public String getDireccion() {
+        return direccion;
+    }
+
+    public void setDireccion(String direccion) {
+        System.out.println("Introduce tu Direccion");
+        this.direccion = direccion;
+    }
+
+    public String getNumeroTelefonico() {
+        return numeroTelefonico;
+    }
+
+    public void setNumeroTelefonico(String numeroTelefonico) {
+        System.out.println("Introduce tu numero telefonico");
+        this.numeroTelefonico = numeroTelefonico;
+    }
+
+    public String getCorreoElectronico() {
+        return correoElectronico;
+    }
+
+    public void setCorreoElectronico(String correoElectronico) {
+        System.out.println("Introduce tu correo Electronico");
+        this.correoElectronico = correoElectronico;
+    }
+}
