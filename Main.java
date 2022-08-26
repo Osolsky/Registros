faae4d3347cc6bbb686df31bc60e57a9c806b9e2
Yael Alfonseca <yaelsaidcecyt03@gmail.com>
2022-08-26 06:56:08 -0500

    ToLowerCase added in switch case clause

--- Main.java
+++ Main.java
@@ -1,25 +1,21 @@
 import java.io.*;
+import java.util.Locale;
 import java.util.Scanner;
 import java.util.Vector;
 
 public class Main {
     public static void main(String[] args) {
         try {
-            //Instancing the objects we will need
-            File workingFile = new File("E:\\Content\\correos.txt");
             Scanner scanner = new Scanner(System.in);
             MasterOfRegister registerControl = new MasterOfRegister();
 
-            //Loop to add whatever data the user wants
             boolean end = false;
             while (!end) {
 
                 registerControl.userComunication();
                 String line = scanner.nextLine();
 
-
-
-                switch (line){
+                switch (line.toLowerCase()){
 
                     case "add":
                         registerControl.addRegister();
